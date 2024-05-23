71a1849f052d4889e60bc327ba90d65b8ed6c5c4
hens <121116111@qq.com>
2024-05-23 18:39:04 +0800

    fate: remove remix part

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { vitePlugin as remix } from '@remix-run/dev'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 export default defineConfig({
-	plugins: [
-		remix({
-			ssr: false,
-			appDirectory: 'src/app',
-		}),
-		tsconfigPaths(),
-	],
+	plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
 })
