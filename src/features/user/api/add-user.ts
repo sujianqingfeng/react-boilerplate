59852911377f28585ce8c15e9498f7093e87f217
hens <121116111@qq.com>
2024-06-03 17:24:59 +0800

    fix: type error

--- src/features/user/api/add-user.ts
+++ src/features/user/api/add-user.ts
@@ -1,10 +1,7 @@
-import { useQueryClient } from '@tanstack/react-query'
 import { api } from '~/lib/api-client'
 
 export const addUser = async (email: string, password: string) => {
 	return api.put('/user/add', { email, password })
 }
 
-export const useAddUser = () => {
-	const queryClient = useQueryClient()
-}
+export const useAddUser = () => {}
